fcd8ba971fb16bc2da3d7ff5886545da11edfd6d
Mans1611 <mans.yousef1611@gmail.com>
2022-10-31 10:43:21 +0200

    creating main branch

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main { 
+    
+    public void run(){
+        Singleton s1 = Singleton.getInstance();
+        System.out.println(s1);
+        
+    }
+    public  static void main(String[] args) {       
+      for(int i = 0 ; i < 20 ; i++){
+         
+
+      }
+    }
+
+}
